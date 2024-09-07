0f55c843abe107a4e839665e9bafefa3a3cb34a7
gaji12345 <gajimax12345@gmail.com>
2024-09-07 10:35:24 +0530

    create ResPonceUTILe class

---
+++ src/main/java/util/ResPonceUTIL.java
@@ -0,0 +1,18 @@
+package util;/*  gaajiCode
+    99
+    07/09/2024
+    */
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ResPonceUTIL {
+    private int code;
+    private String massage;
+    private Object data;
+    
+}
